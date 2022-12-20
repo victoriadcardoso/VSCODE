2d14e5de0c22260c93778c08f7453d2308b26658
victoriadcardoso <victoria.dcardoso@outlook.com>
2022-12-20 17:19:44 -0300

    rascunhos

---
+++ edu/victoria/primeirasemana/BoletimEscolar.java
@@ -0,0 +1,14 @@
+package edu.victoria.primeirasemana;
+
+public class BoletimEscolar {
+    public static void main (String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+
+    }
+}
---
+++ edu/victoria/segundasemana/MinhaClasse.java
@@ -0,0 +1,21 @@
+package edu.victoria.segundasemana;
+public class MinhaClasse {
+    
+
+public static void main (String[] args) {
+
+    String primeiroNome = "Victoria";
+    String segundoNome = "Cardoso";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "resultado do metodo " + primeiroNome.concat (" ").concat(segundoNome);
+
+}
+}
